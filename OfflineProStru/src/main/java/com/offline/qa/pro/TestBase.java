bfa17e58aa9678c5cb5769c0c266dacf8d55d003
shubham <you@example.com>
2020-02-26 08:20:41 +0530

    log4j added

--- OfflineProStru/src/main/java/com/offline/qa/pro/TestBase.java
+++ OfflineProStru/src/main/java/com/offline/qa/pro/TestBase.java
@@ -1,10 +1,11 @@
 package com.offline.qa.pro;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -14,6 +15,9 @@ public class TestBase {
 	
 	public static WebDriver driver;
 	public static Properties prop;
+	public static Logger log;
+
+	
 	
 	public TestBase() throws IOException{
 		prop=new Properties();
@@ -21,18 +25,23 @@ public class TestBase {
 		prop.load(fis);
 	}
 	
-	
+
 	@BeforeMethod
 	public static void OpenBrowser(){
+		
+		log = org.apache.log4j.Logger.getLogger(TestBase.class);
+		PropertyConfigurator.configure(prop.getProperty("C:\\Users\\shubham\\git\\Offline\\OfflineProStru\\src\\main\\resources\\log4j.propertiese"));
 		String brz=prop.getProperty("browser");
 		if(brz.equals("chrome"))
 		{
 		System.setProperty("webdriver.chrome.driver", "chromedriver79.exe");
+		log.info("****Launching Chrome Browser****");
 		driver = new ChromeDriver();
 		}else
 		{
 			String ff=prop.getProperty("firefox");
-			System.out.println("firefox");
+			//System.out.println("firefox");
+			log.info("firefox");
 		}
 		driver.get(prop.getProperty("url"));
 	}
--- OfflineProStru/src/test/java/com/offline/qa/test/LoginPageTest.java
+++ OfflineProStru/src/test/java/com/offline/qa/test/LoginPageTest.java
@@ -10,46 +10,45 @@ import org.testng.annotations.Test;
 import com.offline.qa.pro.TestBase;
 
 public class LoginPageTest extends TestBase {
-	
-	/*@BeforeMethod
-	public void SetuP(){
-		TestBase.OpenBrowser();
-	}*/
 
 	public LoginPageTest() throws IOException {
 		super();
-		
+
 	}
 
-	@Test(priority=1)
-	public void checkTitle(){
+	@Test(priority = 1)
+	public void checkTitle() {
 		String actTitle = driver.getTitle();
 		String expTitle = "JavaByKiran | Log in";
 		Assert.assertEquals(actTitle, expTitle);
-		
+
 	}
-	
-	@Test(priority=2)
-	public void checkErrormsg(){
+
+	@Test(priority = 2)
+	public void checkErrormsg() {
 		driver.findElement(By.id("email")).sendKeys("");
 		driver.findElement(By.xpath("//*[@id=\"form\"]/div[3]/div/button")).click();
-		String actMsg=driver.findElement(By.id("email_error")).getText();
-		System.out.println(actMsg);
-		String expMsg="Please enter email.";
+		String actMsg = driver.findElement(By.id("email_error")).getText();
+		//System.out.println(actMsg);
+		log.info(actMsg);
+		String expMsg = "Please enter email.";
 		Assert.assertEquals(actMsg, expMsg);
-		
+
 	}
-	@Test(priority=3)
-	public void checkErrormsgPassword(){
-		
+
+	@Test(priority = 3)
+	public void checkErrormsgPassword() {
+
 		driver.findElement(By.id("password")).sendKeys("");
 		driver.findElement(By.xpath("//*[@id=\"form\"]/div[3]/div/button")).click();
-		String actMsg=driver.findElement(By.xpath("//*[@id=\"password_error\"]")).getText();
-		System.out.println(actMsg);
-		String expMsg="Please enter password.";
+		String actMsg = driver.findElement(By.xpath("//*[@id=\"password_error\"]")).getText();
+		//System.out.println(actMsg);
+		log.info(actMsg);
+		String expMsg = "Please enter password.";
 		Assert.assertEquals(actMsg, expMsg);
-		
+
 	}
+
 	@Test(priority = 4)
 	public void checkErrormsgBothempty() {
 		driver.findElement(By.id("email")).sendKeys("");
@@ -57,14 +56,15 @@ public class LoginPageTest extends TestBase {
 		driver.findElement(By.xpath("//*[@id=\"form\"]/div[3]/div/button")).click();
 		String actMsg = driver.findElement(By.id("email_error")).getText();
 		String actMsg1 = driver.findElement(By.xpath("//*[@id=\"password_error\"]")).getText();
-		System.out.println(actMsg);
-		System.out.println(actMsg1);
+		//System.out.println(actMsg);
+		//System.out.println(actMsg1);
+		log.info(actMsg);
+		log.debug(actMsg1);
 		String expMsg = "Please enter email.";
 		String expMsg1 = "Please enter password.";
 		Assert.assertEquals(actMsg, expMsg);
 		Assert.assertEquals(actMsg1, expMsg1);
 
 	}
-	
 
 }
